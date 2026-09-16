1967366efd4dc51b9ea2d43884389e73ef702a8a
Maxw_ll <ma.bilang0023@gmail.com>
2026-09-16 17:47:53 +0100

    Added template of classes and declared objects:
    Additonally created a venv of 3.12 since this code only works on that version of python

--- main.py
+++ main.py
@@ -1,2 +1,55 @@
-print("First test commit")
-print("hello")
+#Modules
+from PyQt5.QtWidgets import QApplication, QWidget, QTextEdit, QComboBox, QPushButton, QLabel, QHBoxLayout, QVBoxLayout
+from googletrans import Translator
+from languages import *
+
+#Class
+class Home(QWidget):
+    #Constructor
+    def __init__(self):
+        super().__init__()
+
+
+    # Objects and UI Layout
+    def initUI(self):
+        self.input_box = QTextEdit()
+        self.output_box = QTextEdit()
+        self.reverse = QPushButton("Reverse")
+        self.reset = QPushButton("Reset")
+        self.submit = QPushButton("Translate Now")
+        self.input_option = QComboBox()
+        self.output_option = QComboBox()
+        self.title = QLabel("PyLate")
+
+
+    def settings(self):
+        pass
+
+
+    def button_click(self):
+        pass
+
+
+    def translate_click(self):
+        pass
+
+
+    def reset_app(self):
+        pass
+
+
+    def translate_text(self):
+        pass
+
+
+    def reverse(self):
+        pass
+
+
+
+#Main Run "I think this is standard procedure"
+if __name__ == "__main__":
+    app = QApplication([])
+    main = Home()
+    main.show()
+    app.exec_()
