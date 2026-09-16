642cb510a98f6575a92f790e7c3c543ec609aeb2
Maxw_ll <ma.bilang0023@gmail.com>
2026-09-16 18:45:04 +0100

    Implemented a layout and added widgets to the layout. Implemented the translate_text and settings function

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ class Home(QWidget):
     #Constructor
     def __init__(self):
         super().__init__()
+        self.initUI()
+        self.settings()
 
 
     # Objects and UI Layout
@@ -19,11 +21,41 @@ class Home(QWidget):
         self.submit = QPushButton("Translate Now")
         self.input_option = QComboBox()
         self.output_option = QComboBox()
+
+        self.input_option.addItems(values)
+        self.output_option.addItems(values)
+
+
         self.title = QLabel("PyLate")
+
+        self.master = QHBoxLayout()
+
+        col1 = QVBoxLayout()
+        col2 = QVBoxLayout()
+
+        # Declares objects(widgets) to the layout
+        col1.addWidget(self.title)
+        col1.addWidget(self.input_option)
+        col1.addWidget(self.output_option)
+        col1.addWidget(self.submit)
+        col1.addWidget(self.reset)
+
+        col2.addWidget(self.input_box)
+        col2.addWidget(self.reverse)
+        col2.addWidget(self.output_box)
+
+        # Actually adds the columns/objects to the main layout
+        self.master.addLayout(col1,20)
+        self.master.addLayout(col2,80)
+
+        self.setLayout(self.master)
 
 
     def settings(self):
-        pass
+        self.setWindowTitle("PyLate")
+
+        #takes x,y,width,height of the window and sets it to the window
+        self.setGeometry(250,250,800,400)
 
 
     def button_click(self):
@@ -31,15 +63,25 @@ class Home(QWidget):
 
 
     def translate_click(self):
-        pass
+        value_to_key1 = self.output_option.currentText()
+        value_to_key2 = self.input_option.currentText()
+
+        # Gets the key from the value in the dictionary
+        key_to_value1 = [key for key, value in LANGUAGES.items() if value == value_to_key1][0]
 
 
     def reset_app(self):
         pass
 
 
-    def translate_text(self):
-        pass
+    # Uses googletrans library to translate the text from the input box to the output box
+    # Takes in the text, destination language, and source language as parameters
+    def translate_text(self, text, dest, src):
+        # Translator is the class from the googletrans module that allows us to translate text
+        # Speaker is just the object
+        speaker = Translator()
+        translation = speaker.translate(text, dest=dest, src=src)
+        return translation.text
 
 
     def reverse(self):
